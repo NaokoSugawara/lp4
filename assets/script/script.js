89872702e47aff7144432d80c216219007e30b66
naoko.sugawara <infoseek122@gmail.com>
2025-02-11 18:17:11 -0500

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,95 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    // carousel1
+    const splide = new Splide('#splide1', {
+        type   : 'loop',
+        perPage: 1,      
+        perMove: 1,      
+        autoplay: true,
+        interval: 2000, 
+        pagination: false,
+        arrows: false,
+        breakpoints: {   
+            1024: { perPage: 2 },
+            767: { type: 'loop', perPage: 1, padding: '5%', gap: '10%', }
+        },
+    })
+
+
+    // progress bar -----------------------------------------------------
+    const bar = splide.root.querySelector( '.my-carousel-progress-bar' );
+
+    // Updates the bar width whenever the carousel moves:
+    splide.on( 'mounted move', function () {
+        const end  = splide.Components.Controller.getEnd() + 1;
+        const rate = Math.min( ( splide.index + 1 ) / end, 1 );
+        bar.style.width = String( 100 * rate ) + '%';
+    } );
+    // ------------------------------------------------------------------
+
+    splide.mount();
+
+
+    // carousel2
+    const splide2 = new Splide('#splide2', {
+        type   : 'loop',
+        perPage: 1,      
+        perMove: 1,      
+        autoplay: true,
+        interval: 2000, 
+        pagination: false,
+        arrows: false,
+        breakpoints: {   
+            1024: { perPage: 2 },
+            767: { type: 'loop', perPage: 1, padding: '5%', gap: '10%', }
+        },
+    })
+
+
+    // progress bar -----------------------------------------------------
+    const bar2 = splide2.root.querySelector( '.my-carousel-progress-bar2' );
+
+    // Updates the bar width whenever the carousel moves:
+    splide2.on( 'mounted move', function () {
+        const end  = splide2.Components.Controller.getEnd() + 1;
+        const rate = Math.min( ( splide2.index + 1 ) / end, 1 );
+        bar2.style.width = String( 100 * rate ) + '%';
+    } );
+    // ------------------------------------------------------------------
+
+    splide2.mount();
+
+
+    // carousel3
+    const splide3 = new Splide('#splide3', {
+        type   : 'loop',
+        perPage: 1,      
+        perMove: 1,      
+        autoplay: true,
+        interval: 2000, 
+        pagination: false,
+        arrows: false,
+        breakpoints: {   
+            1024: { perPage: 2 },
+            767: { type: 'loop', perPage: 1, padding: '5%', gap: '10%', }
+        },
+    })
+
+
+    // progress bar -----------------------------------------------------
+    const bar3 = splide3.root.querySelector( '.my-carousel-progress-bar3' );
+
+    // Updates the bar width whenever the carousel moves:
+    splide3.on( 'mounted move', function () {
+        const end  = splide3.Components.Controller.getEnd() + 1;
+        const rate = Math.min( ( splide3.index + 1 ) / end, 1 );
+        bar3.style.width = String( 100 * rate ) + '%';
+    } );
+    // ------------------------------------------------------------------
+
+    splide3.mount();
+});
+
