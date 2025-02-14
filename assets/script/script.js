c152e5394a96c31d9e9a91730f161e1c43cdad28
naoko.sugawara <infoseek122@gmail.com>
2025-02-13 23:38:45 -0500

    fixed header on top / added accordion to q and a

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,27 @@
 
+//
+// Accordion
+//
+
+const accordionTitles = document.querySelectorAll(".accordion__title");
+
+accordionTitles.forEach((accordionTitle) => {
+
+    accordionTitle.addEventListener("click", () => {
+        const icon = accordionTitle.children[1];
+        icon.classList.toggle("accordion__title__icon--active");
+
+        let height = accordionTitle.nextElementSibling.scrollHeight;
+        if (icon.classList.contains("accordion__title__icon--active")) {
+            accordionTitle.nextElementSibling.style.maxHeight = `${height}px`;
+        } else {
+            accordionTitle.nextElementSibling.style.maxHeight = "0px";
+        }
+    })
+
+})
+
+
 
 
 document.addEventListener('DOMContentLoaded', function () {
