aca7d9e0fe8c78d37692659a503df494fc676053
naoko.sugawara <infoseek122@gmail.com>
2025-02-21 20:21:46 -0500

    fixed 2nd page for sp

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,11 @@
 
+
+// header inclusion
+
 fetch("header.html")
 .then(response => response.text())
 .then(data => document.getElementById("header-placeholder").innerHTML = data);
+
 
 
 //
@@ -40,10 +44,9 @@ document.addEventListener('DOMContentLoaded', function () {
         interval: 2000,
         pagination: false,
         arrows: false,
-        // breakpoints: {
-        //     1024: { perPage: 2 },
-        //     767: { type: 'loop', perPage: 1, padding: '5%', gap: '10%', }
-        // },
+        breakpoints: {
+            767: { pagination: true, }
+        },
     })
 
 
@@ -61,128 +64,18 @@ document.addEventListener('DOMContentLoaded', function () {
     splide.mount();
 
 
-
-    // carousel2
-    const splide2 = new Splide('#splide2', {
-        type   : 'loop',
-        perPage: 1,
-        perMove: 1,
-        // autoplay: true,
-        // interval: 2000,
-        pagination: false,
-        arrows: false,
-    })
-
-
     // progress bar -----------------------------------------------------
-    const bar2 = splide2.root.querySelector( '.my-carousel-progress-bar2' );
+    // const bar4 = splide4.root.querySelector( '.my-carousel-progress-bar4' );
 
-    // Updates the bar width whenever the carousel moves:
-    splide2.on( 'mounted move', function () {
-        const end  = splide2.Components.Controller.getEnd() + 1;
-        const rate = Math.min( ( splide2.index + 1 ) / end, 1 );
-        bar2.style.width = String( 100 * rate ) + '%';
-    } );
+    // // Updates the bar width whenever the carousel moves:
+    // splide4.on( 'mounted move', function () {
+    //     const end  = splide4.Components.Controller.getEnd() + 1;
+    //     const rate = Math.min( ( splide4.index + 1 ) / end, 1 );
+    //     bar4.style.width = String( 100 * rate ) + '%';
+    // } );
     // ------------------------------------------------------------------
 
-    splide2.mount();
 
-
-
-    // carousel3
-    const splide3 = new Splide('#splide3', {
-        type   : 'loop',
-        perPage: 1,
-        perMove: 1,
-        autoplay: false,
-        interval: 2000,
-        pagination: false,
-        arrows: false,
-        // breakpoints: {
-        //     1024: { perPage: 2 },
-        //     767: { type: 'loop', perPage: 1, padding: '5%', gap: '10%', }
-        // },
-    })
-
-
-    // progress bar -----------------------------------------------------
-    const bar3 = splide3.root.querySelector( '.my-carousel-progress-bar3' );
-
-    // Updates the bar width whenever the carousel moves:
-    splide3.on( 'mounted move', function () {
-        const end  = splide3.Components.Controller.getEnd() + 1;
-        const rate = Math.min( ( splide3.index + 1 ) / end, 1 );
-        bar3.style.width = String( 100 * rate ) + '%';
-    } );
-    // ------------------------------------------------------------------
-
-    splide3.mount();
-
-
-
-    // carousel4
-    const splide4 = new Splide('#splide4', {
-        type   : 'loop',
-        perPage: 3,
-        gap: '3%',
-        // autoplay: true,
-        // interval: 2000,
-        pagination: false,
-        arrows: false,
-        breakpoints: {
-            850: { gap: '10%', },
-            767: { perPage: 1 }
-        },
-    })
-
-
-    // progress bar -----------------------------------------------------
-    const bar4 = splide4.root.querySelector( '.my-carousel-progress-bar4' );
-
-    // Updates the bar width whenever the carousel moves:
-    splide4.on( 'mounted move', function () {
-        const end  = splide4.Components.Controller.getEnd() + 1;
-        const rate = Math.min( ( splide4.index + 1 ) / end, 1 );
-        bar4.style.width = String( 100 * rate ) + '%';
-    } );
-    // ------------------------------------------------------------------
-
-    splide4.mount();
-
-
-    // carousel5
-    const splide5 = new Splide('#splide5', {
-        type   : 'loop',
-        perPage: 3,
-        gap: '3%',
-        autoplay: true,
-        interval: 2000,
-        pagination: false,
-        arrows: false,
-        breakpoints: {
-            850: { gap: '10%', },
-            767: { perPage: 1 }
-        },
-    })
-
-    splide5.mount();
-
-        // carousel6
-        const splide6 = new Splide('#splide6', {
-            type   : 'loop',
-            perPage: 3,
-            gap: '3%',
-            autoplay: true,
-            interval: 2000,
-            pagination: false,
-            arrows: false,
-            breakpoints: {
-                850: { gap: '10%', },
-                767: { perPage: 1 }
-            },
-        })
-
-        splide6.mount();
 
 });
 
